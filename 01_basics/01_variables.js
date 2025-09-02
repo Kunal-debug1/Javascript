65049417527fc27a156ed5e7180ee7275ac4dc25
Kunal Gaikwad <kunalgaikwad.exe@gmail.com>
2025-09-02 09:02:07 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345678
+let accountEmail="kunalgaikwad@gmail.com"
+var accountPassword ="12345678"
+accountCity="Aurangabad"
+let accountState;
+
+//console.log(accountId);
+
+/*accountId = 123 not allowed because it is constant value*/
+accountEmail="kunal@gmail.com"
+accountPassword ="123484678"
+accountCity="Jalna"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+// variable declare using three methods 1.const = which is for constant value.
+//2. let which is for the varibles
+//3.var which is not preferd because of the block scope and functional problems.
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+alert(3 + 3) // we are using nodejs , not browser
+
+// code readability should be high
+
+let name ="Kunal"
+let age=21
+let isLoggedIn = false
+
+// number=> 2 to power 53
+// bigInt 
+// strings = '',""
+// boolean = true/false
+// null = standalone value
+// undefined = 
+// symbol= unique
+
+//object 
+
+consol.log(typeof age);
