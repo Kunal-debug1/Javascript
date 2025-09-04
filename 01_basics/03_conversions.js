4476d74c54f4a050c5fe90b327f34c88b51a9f63
Kunal Gaikwad <kunalgaikwad.exe@gmail.com>
2025-09-04 06:02:01 +0000

    conversion of data types

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,28 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valuInNumber =Number(score)
+console.log(typeof valuInNumber);
+console.log(valuInNumber);
+
+
+//"33" -> 33
+//"333as" -> NaN 
+//true -> 1; false -> 0
+// null -> o;
+// undefined -> NaN
+
+let isLoggedIn =""
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1-> true;  0-> false
+// "" ->  false
+// "kunal" -> true
+
+let someNum=45
+let stringNum = String(someNum)
+console.log(stringNum)
+console.log(typeof stringNum)
