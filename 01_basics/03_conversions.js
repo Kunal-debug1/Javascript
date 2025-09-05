96ab76a520589ab6c58cc0dd85896937d31c4798
Kunal Gaikwad <kunalgaikwad.exe@gmail.com>
2025-09-05 05:41:57 +0000

    update

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -1,28 +1,74 @@
-let score = undefined
+// let score = undefined
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valuInNumber =Number(score)
-console.log(typeof valuInNumber);
-console.log(valuInNumber);
+// let valuInNumber =Number(score)
+// console.log(typeof valuInNumber);
+// console.log(valuInNumber);
 
 
-//"33" -> 33
-//"333as" -> NaN 
-//true -> 1; false -> 0
-// null -> o;
-// undefined -> NaN
+// //"33" -> 33
+// //"333as" -> NaN 
+// //true -> 1; false -> 0
+// // null -> o;
+// // undefined -> NaN
 
-let isLoggedIn =""
-let booleanIsLoggedIn =Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn =""
+// let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1-> true;  0-> false
-// "" ->  false
-// "kunal" -> true
+// // 1-> true;  0-> false
+// // "" ->  false
+// // "kunal" -> true
 
-let someNum=45
-let stringNum = String(someNum)
-console.log(stringNum)
-console.log(typeof stringNum)
+// let someNum=45
+// let stringNum = String(someNum)
+// console.log(stringNum)
+// console.log(typeof stringNum)
+
+// //***************************************************************************************************** */
+
+// let value=3
+// let negValue = -value
+// console.log(negValue);
+
+//Arthmetic Operator
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/9)
+// console.log(2**2)
+// console.log(2%4)
+
+
+// let  str1="Kunal"
+// let str2="Gaikwad"
+
+// let str3= str1 + ' ' + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(typeof(1 + "5"))
+// console.log("1" + 2 + 6)
+// console.log(1 + 2 + "6")
+
+// console.log(true);
+// console.log(+true);
+
+// // console.log(true+); error
+
+// console.log(+"");
+
+let num1, num2, num3
+num1=num2=num3=4;
+console.log(num1);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter1 = 100
+++gameCounter1;
+console.log(gameCounter1);
