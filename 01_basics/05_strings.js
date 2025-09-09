86db007ea9ca313dddf9d70e59a02e80ecc94ba5
Kunal Gaikwad <kunalgaikwad.exe@gmail.com>
2025-09-09 09:44:10 +0000

    update

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+// const name = "Kunal"
+// const surname = "Gaikwad"
+// let age = 21
+
+// console.log(name + surname + age)
+
+// console.log(`my name is ${name + surname} and i'm ${age} years old`);
+
+// // strings methods:
+const gameName = new String("Kunal-raju-Gaikwad")
+// console.log(gameName);
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('d'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString)
+
+// const anotherString = gameName.slice(-9,6)
+// console.log(anotherString);
+
+
+//trim removes specail CharacterData, spaces
+const hiKunal = "  kunal  "
+console.log(hiKunal)
+console.log(hiKunal.trim());
+
+const url ="https://kunal.com/kunalportfolio578ksijd"
+
+console.log(url.replace('578','-'));
+
+console.log(url.includes("kunal"))
+console.log(gameName.split('-'));
+console.log(gameName.split(''));
