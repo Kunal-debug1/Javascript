7e183d1ac09c4d4d5cd145046a63330a7fdb9316
Kunal Gaikwad <kunalgaikwad.exe@gmail.com>
2025-12-13 19:49:17 +0000

    update

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,43 @@
-console.log("HI Kunal");
+// Store your name, age, and city in variables and print them.
+
+const name = "Kunal Gaikwad";
+let age = 22;
+let city = "Hyderabad"
+
+console.log(`my name is ${name}, I'm ${age} year old. I live in ${city}`)
+
+
+//Write a program that adds two numbers.
+
+const a = 100;
+const b = 200;
+
+let result = a + b;
+console.log(result);
+
+//Convert a string "50" into a number and add 20.
+
+let d = "50";
+let e = 20;
+
+result = Number(d) + e ;
+console.log(result);
+console.log(typeof(result));
+
+//Use a ternary operator to check if a number is even or odd.
+
+let num = 26;
+
+result = (num % 2 == 0) ? "even" : "odd" ;
+
+console.log(result);
+
+
+//Print:
+
+//Hello, my name is ___ and I live in ___.
+
+const Name = "Kunal Gaikwad";
+let City = "Hyderabad"
+
+console.log(`my name is ${Name} and I live in ${City}`)
