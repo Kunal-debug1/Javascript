0732c477be922f9909e25433d3c3818fc36d0c0c
Kunal Gaikwad <kunalgaikwad.exe@gmail.com>
2025-12-13 20:48:24 +0000

    update

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,6 +1,5 @@
 // Array
-
-const myArr = [1,2,3,4,5]
+let myArr = [0,1,2,3,4,5]
 //console.log(myArr)
 
 // const myHeros = ["Ironman","Thor","Human"]
@@ -11,13 +10,31 @@ const myArr = [1,2,3,4,5]
 
 
 
-// Array Methods
+// **Array Methods**
 
 // myArr.push(8)  //adds the value at the end of array
 // myArr.push(7)
-//myArr.pop()  //removes value from the end
+// myArr.pop()  //removes value from the end
 
-myArr.unshift(9)  // adds the value at the start
+//myArr.unshift(9)  // adds the value at the start
+//myArr.shift()       // reomves values from first
+// console.log(myArr.includes(5)); // result bools=ean values
+// console.log(myArr.indexOf(4)); // shows the index of given number
 
-console.log(myArr);
+// const newArr = myArr.join()   //
+// console.log(typeof newArr);
+// console.log(myArr);
 
+// // Slice and Splice
+
+// console.log("A", myArr);
+
+// const myn1 = myArr.slice(1,3) //doesn't manipulate array 
+// console.log(myn1);
+// console.log("B" , myArr);
+
+// const myn2 = myArr.splice(1,3) //manipulate array an removes the  range  value 
+// console.log(myn2);
+// console.log("C",myArr);
+
+ 
